7c1cad7ce0a924600f035e15b85f700c4be6ffd8
Jaividhyarthi <141346602+Jaividhyarthi@users.noreply.github.com>
2026-05-19 10:19:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -230,4 +230,10 @@ def clear_all():
     return jsonify({'status': 'cleared'})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
+
+
+
+
+
+
